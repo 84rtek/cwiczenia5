63623c3d53184a477b64065a7a50b4107f5a4148
84rtek <128226924+84rtek@users.noreply.github.com>
2023-06-23 22:59:36 +0200

    Add files via upload

---
+++ iteracja-zadanie3.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Liczba wierszy: ");
+        int n = int.Parse(Console.ReadLine());
+
+
+        for (int i = 0; i <= n; i++)
+        {
+            for (int j = 1; j <= n - i; j++)
+                Console.Write("   ");
+            for (int j = 1; j <= 2 * i - 1; j++)
+                Console.Write(" + ");
+            Console.Write("\n");
+        }
+    }
+}
